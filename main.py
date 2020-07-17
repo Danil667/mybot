41ad548ee5fb8a7ab060a8757d5a9e687793db92
danil732000@gmail.com <danil732000@gmail.com>
2020-07-17 13:53:26 +0400

    fix

--- main.py
+++ main.py
@@ -1,17 +1,14 @@
-#!venv/bin/python
-import logging
-import config
-import requests
+from telebot import types
 import random
-from aiogram import Bot, Dispatcher, executor, types
+import config
+import telebot
+import requests
 from bs4 import BeautifulSoup as BS
-logging.basicConfig(level=logging.INFO)
 
-bot = Bot(config.TOKEN)
-dp = Dispatcher(bot)
-
-r = requests.get('https://sinoptik.ua/погода-ульяновск/')
+r = requests.get('https://sinoptik.ua/погода-ульяновск')
 html = BS(r.content, 'html.parser')
+bot = telebot.TeleBot(config.TOKEN)
+
 for el in html.select('#content'):
     temp_now = el.select('.temperature .cur')[0].text
     temp_max = el.select('.temperature .max')[0].text
@@ -21,46 +18,50 @@ layout = dict(zip(map(ord, "qwertyuiop[]asdfghjkl;'zxcvbnm,./`"
                            'QWERTYUIOP{}ASDFGHJKL:"ZXCVBNM<>?~'),
                            "йцукенгшщзхъфывапролджэячсмитьбю.ё"
                            'ЙЦУКЕНГШЩЗХЪФЫВАПРОЛДЖЭЯЧСМИТЬБЮ,Ё'))
-# /start
-@dp.message_handler(commands=["start"])
-async def cmd_start(message: types.Message):
+
+@bot.message_handler(commands=["start"])
+def main(message):
     menu_keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
     menu_keyboard.add(types.KeyboardButton(text="Прогноз погоды ⛅"))
     menu_keyboard.add(types.KeyboardButton(text="Рандом 🎲"))
     menu_keyboard.add(types.KeyboardButton(text="Перевод раскладки"))
     menu_keyboard.add(types.KeyboardButton(text="Помощь"))
     menu_keyboard.add(types.KeyboardButton(text="Отмена"))
-    await message.answer("Используйте кнопки ниже для взаимодействия с ботом!", reply_markup=menu_keyboard)
+    bot.send_message(message.chat.id,"Используйте кнопки ниже для взаимодействия с ботом!", reply_markup=menu_keyboard)
 
-#help
-@dp.message_handler(commands=["help"])
-async def welcome(message):
+@bot.message_handler(commands=["help"])
+def welcome(message):
     start_keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
-    await message.answer("Приветствую тебя, <strong>{0.first_name}</strong>! \n\nМеня зовут <b>danila73bot</b>\n\nИ вот что я могу: \n•Покзать температуру в Ульяновске ⛅ \n•Выбрать рандомное число 🎲 \n•Перевести текст с английской на русскую раскладку \n Для работы используйте меню".format(message.from_user), parse_mode='html')
-    await message.answer("Введите /start чтобы начать работу 😎")
+    bot.send_message(message.chat.id,"Приветствую тебя, <strong>{0.first_name}</strong>! \n\nМеня зовут <b>danila73bot</b>\n\nИ вот что я могу: \n•Покзать температуру в Ульяновске ⛅ \n•Выбрать рандомное число 🎲 \n•Перевести текст с английской на русскую раскладку \n Для работы используйте меню".format(message.from_user), parse_mode='html')
+    bot.send_message(message.chat.id,"Введите /start чтобы начать работу 😎")
 
-#“Отмена”
-@dp.message_handler(lambda message: message.text == "Отмена")
-async def action_cancel(message: types.Message):
+
+@bot.message_handler(content_types = ['text'])
+def keyboard_answer(message):
+    if message.text == 'Прогноз погоды ⛅':
+        bot.send_message(message.chat.id,
+                               "Прогноз погоды в городе Ульяновск на данный момент и максимальная:\n" + 'Сейчас ' + temp_now + ', ' + temp_max + '\n' + text)
+    elif message.text == 'Рандом 🎲':
+        bot.send_message(message.chat.id, str(random.randint(1, 1000)))
+    elif message.text == 'Помощь':
+        welcome(message)
+    elif message.text == 'Отмена':
+        action_cancel(message)
+    elif message.text == 'Перевод раскладки':
+        bot.send_message(message.chat.id, 'Отлично! Введи text')
+        bot.register_next_step_handler(message, text_translate)
+    else:
+        bot.send_message(message.chat.id, "Я вас не понимаю \nНе забывайте про меню с коммандами /start")
+
+@bot.message_handler(lambda message: message.text == "Отмена")
+def action_cancel(message: types.Message):
     remove_keyboard = types.ReplyKeyboardRemove()
-    await message.answer("Действие отменено. Введите /start, чтобы начать заново.", reply_markup=remove_keyboard)
-
-@dp.message_handler(content_types=['text'])
-async def keyboard_answer(message: types.message):
-    if message.chat.type == 'private':
-        if message.text == 'Прогноз погоды ⛅':
-            await bot.send_message(message.chat.id, "Прогноз погоды в городе Ульяновск на данный момент и максимальная:\n" + 'Сейчас ' + temp_now + ', ' + temp_max + '\n' + text)
-        elif message.text == 'Рандом 🎲':
-            await bot.send_message(message.chat.id, str(random.randint(1, 1000)))
-        elif message.text == 'Помощь':
-            await welcome(message)
-        elif message.text == 'Перевод раскладки':
-            await bot.send_message(message.chat.id, "Введите текст")
-        else:
-            await bot.send_message(message.chat.id, message.text.translate(layout))
-            await bot.send_message(message.chat.id, "Не забывайте про меню с другими коммандами")
+    bot.send_message(message.chat.id,"Действие отменено. Введите /start, чтобы начать заново.", reply_markup=remove_keyboard)
 
 
+def text_translate(message):
+    bot.send_message(message.chat.id, message.text.translate(layout))
 
-if __name__ == "__main__":
-    executor.start_polling(dp, skip_updates=True)
+
+if __name__ == '__main__':
+    bot.polling(none_stop=True)
